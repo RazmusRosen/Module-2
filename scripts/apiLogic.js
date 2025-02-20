56a5eed0ffd104b00550da3a74c033d5a0737455
Razmus R <Razmus.Rosen@hotmail.com>
2025-02-20 21:24:24 +0100

    Removed some fetch request, added try catch and some small fixes across the code

--- scripts/apiLogic.js
+++ scripts/apiLogic.js
@@ -1,13 +1,17 @@
 fetchPosts();
 
 async function fetchPosts() {
-    let result = await fetch('https://dummyjson.com/posts')
-    result = await result.json();
-    createPosts(result.posts);
+    try {
+        let result = await fetch('https://dummyjson.com/posts?limit=0')
+        result = await result.json();
+        createPosts(result.posts);
+    } catch (error) {
+        console.log(error, 'An error occured while fetching posts.');
+    }
 }
 
-async function createPostUser(user) {
-    let postUser = `<img src=${user.image}><a class="clickable-user" onclick="profileMenu(event)">${user.username}
+function createPostUser(user) {
+    let postUser = `<img src=${user.image}><a class="clickable-user" onclick="profileMenu(event)">${user.username} ${user.id}
         <div class="user-profile">
             Name:${user.firstName} ${user.lastName}
             Email:${user.email}
@@ -21,26 +25,15 @@ async function createPostUser(user) {
 async function createPosts(posts) {
     let section = document.querySelector('section');
     for (let i = 0; i < posts.length; i++) {
-        let user = await getUser(posts[i].id);
-        /* or do you mean like this?
-        let post = {
-            id: posts[i].id,
-            title: posts[i].title,
-            body: posts[i].body,
-            reactions: {
-                likes: posts[i].reactions.likes,
-                dislikes: posts[i].reactions.dislikes
-            }
-        }
-        */
-       let postArticle = document.createElement('article');
+        let user = await getUser(posts[i].userId);
+        let postArticle = document.createElement('article');
         postArticle.innerHTML = `
         <div class="post-user">
-        ${await createPostUser(user)}
+        ${createPostUser(user)}
         </div>
         <h2>${posts[i].title}</h2>
         <p>${posts[i].body}</p>
-        <p>${await getTags(posts[i].id)}</p>
+        <p>${createTags(posts[i].tags)}</p>
         <p><img src="thumpUp.gif">${posts[i].reactions.likes} <img src="thumpDown.gif">${posts[i].reactions.dislikes}</p>
         <div>
         <h3>Comments</h3>
@@ -66,58 +59,64 @@ async function getUser(postId) {
         username: "",
         image: ""
     }
-    let result = await fetch(`https://dummyjson.com/user/${postId}`)
+    try {
+        let result = await fetch(`https://dummyjson.com/user/${postId}`)
+        result = await result.json();
+        user.id = result.id;
+        user.firstName = result.firstName;
+        user.lastName = result.lastName;
+        user.age = result.age;
+        user.gender = result.gender;
+        user.email = result.email;
+        user.phone = result.phone;
+        user.username = result.username;
+        user.image = result.image;
 
-    result = await result.json();
-    user.id = result.id;
-    user.firstName = result.firstName;
-    user.lastName = result.lastName;
-    user.age = result.age;
-    user.gender = result.gender;
-    user.email = result.email;
-    user.phone = result.phone;
-    user.username = result.username;
-    user.image = result.image;
+        return user;
 
-    return user;
+    } catch (error) {
+        console.log(error, `An error occured while fetching user data with id ${postId}.`);
+    }
 }
 
-async function getTags(postId) {
-    let result = await fetch(`https://dummyjson.com/posts/${postId}`)
-    result = await result.json();
+function createTags(tagsArray) {
     let tags = "";
-    for (let i = 0; i < result.tags.length; i++) {
-        tags += `#${result.tags[i]} `;
+    for (let i = 0; i < tagsArray.length; i++) {
+        tags += `#${tagsArray[i]} `;
     }
     return tags;
 }
 
 async function getComments(postId) {
-    let result = await fetch(`https://dummyjson.com/posts/${postId}/comments`)
-    result = await result.json();
-
-    let comments = "";
-    for (let i = 0; i < result.comments.length; i++) {
-        /* or do you mean like this?
-        user.username = result.comments[i].user.username;
-        user.body = result.comments[i].body;
-        user.likes = result.comments[i].likes;
-        */
-       let user = await getUser(result.comments[i].user.id);
-
-        comments += `<li><a class="clickable-user" onclick="profileMenu(event)">${user.username}
-        <div class="user-profile">
-            Name:${user.firstName} ${user.lastName}
-            Email:${user.email}
-            Gender:${user.gender}
-        </div>
-        </a>: ${result.comments[i].body} <img src="thumpUp.gif">${result.comments[i].likes}</li>`;
+    try {
+        let result = await fetch(`https://dummyjson.com/posts/${postId}/comments`)
+        result = await result.json();
+        let comments = createComments(result)
+        return comments;
+    } catch (error) {
+        console.log(error, `An error occured while fetching comments for post with id ${postId}.`);
     }
-    if (comments === "") {
-        comments = "No comments available.";
-    }
-    return comments;
 }
+
+    async function createComments(result) {
+        let comments = "";
+        for (let i = 0; i < result.comments.length; i++) {
+            
+        let user = await getUser(result.comments[i].user.id);
+
+            comments += `<li><a class="clickable-user" onclick="profileMenu(event)">${user.username}
+            <div class="user-profile">
+                Name:${user.firstName} ${user.lastName}
+                Email:${user.email}
+                Gender:${user.gender}
+            </div>
+            </a>: ${result.comments[i].body} <img src="thumpUp.gif">${result.comments[i].likes}</li>`;
+        }
+        if (comments === "") {
+            comments = "<li>No comments available.</li>";
+        }
+        return comments;
+    }
 
 function profileMenu(event) {
     let profile = event.target.closest('.clickable-user').querySelector('.user-profile');
