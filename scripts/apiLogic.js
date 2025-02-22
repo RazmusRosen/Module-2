4ec4334b96abeb10a5e06d8905fb886feb23e1a8
Razmus R <Razmus.Rosen@hotmail.com>
2025-02-22 13:12:34 +0100

    Removed the innerHTML from createPost and added createElement instead and append, added DocumentFragment as well

--- scripts/apiLogic.js
+++ scripts/apiLogic.js
@@ -11,7 +11,7 @@ async function fetchPosts() {
 }
 
 function createPostUser(user) {
-    let postUser = `<img src=${user.image}><a class="clickable-user" onclick="profileMenu(event)">${user.username} ${user.id}
+    let postUser = `<img src=${user.image} alt="User-profile picture."><a class="clickable-user" onclick="profileMenu(event)">${user.username}
         <div class="user-profile">
             Name:${user.firstName} ${user.lastName}
             Email:${user.email}
@@ -24,26 +24,46 @@ function createPostUser(user) {
 
 async function createPosts(posts) {
     let section = document.querySelector('section');
+    
     for (let i = 0; i < posts.length; i++) {
         let user = await getUser(posts[i].userId);
         let postArticle = document.createElement('article');
-        postArticle.innerHTML = `
-        <div class="post-user">
-        ${createPostUser(user)}
-        </div>
-        <h2>${posts[i].title}</h2>
-        <p>${posts[i].body}</p>
-        <p>${createTags(posts[i].tags)}</p>
-        <p><img src="thumpUp.gif">${posts[i].reactions.likes} <img src="thumpDown.gif">${posts[i].reactions.dislikes}</p>
-        <div>
-        <h3>Comments</h3>
-        <ul>
-        ${await getComments(posts[i].id)}
-        </ul>
-        </div>
-        `;
+        
+        let postUser = document.createElement('div');
+        postUser.classList.add('post-user');
+        postUser.innerHTML = createPostUser(user);
 
-        section.appendChild(postArticle);
+        let postContent = document.createElement('div');
+        postContent.classList.add('post-content');
+        let title = document.createElement('h2');
+        title.textContent = posts[i].title;
+        let postText = document.createElement('p');
+        postText.textContent = posts[i].body;
+        let tags = document.createElement('p');
+        tags.textContent = createTags(posts[i].tags);
+        let reactions = document.createElement('p');
+        reactions.innerHTML = `<img src="thumpUp.gif" alt="A picture of a thumps up to show likes.">${posts[i].reactions.likes} <img src="thumpDown.gif" alt="A picture of a thumps down to show dislikes.">${posts[i].reactions.dislikes}`;
+        postContent.appendChild(title);
+        postContent.appendChild(postText);
+        postContent.appendChild(tags);
+        postContent.appendChild(reactions);
+
+        let comment = document.createElement('div');
+        let commentTitle = document.createElement('h3');
+        commentTitle.textContent = "Comments";
+        let commentList = document.createElement('ul');
+        let comments = await getComments(posts[i].id);
+        commentList.innerHTML = comments;
+        comment.appendChild(commentTitle);
+        comment.appendChild(commentList);
+
+        postArticle.appendChild(postUser);
+        postArticle.appendChild(postContent);
+        postArticle.appendChild(comment);
+
+        let fragment = document.createDocumentFragment();
+        fragment.appendChild(postArticle);
+        section.appendChild(fragment);
     }        
 }
 
@@ -103,14 +123,17 @@ async function getComments(postId) {
         for (let i = 0; i < result.comments.length; i++) {
             
         let user = await getUser(result.comments[i].user.id);
+            //maybe have one same function for create user and the comments as i did above or the extra info as
+            //a function that i call for comments and post-user
 
+            //nog samma här med createelement osv
             comments += `<li><a class="clickable-user" onclick="profileMenu(event)">${user.username}
             <div class="user-profile">
                 Name:${user.firstName} ${user.lastName}
                 Email:${user.email}
                 Gender:${user.gender}
             </div>
-            </a>: ${result.comments[i].body} <img src="thumpUp.gif">${result.comments[i].likes}</li>`;
+            </a>: ${result.comments[i].body} <img src="thumpUp.gif" alt="A picture of a thumps up to show likes.">${result.comments[i].likes}</li>`;
         }
         if (comments === "") {
             comments = "<li>No comments available.</li>";
